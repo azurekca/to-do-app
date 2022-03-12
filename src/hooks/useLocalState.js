ac745bc4e76b1a5e4c7329e7785d81468e0c5129
Agata Zurek <agata.w.zurek@gmail.com>
2022-03-12 15:51:22 -0800

    feat: hook for state from local storage

--- src/hooks/useLocalState.js
+++ src/hooks/useLocalState.js
@@ -1,5 +1,22 @@
-
-
+import { useEffect, useState } from 'react';
+/**
+ * Use local storage to save state across sessions
+ * adapted from https://www.joshwcomeau.com/snippets/react-hooks/use-sticky-state/
+ * @param {any} initState default value
+ * @param {string} localKey used for the local storage key
+ * @returns {any}
+ */
 export default function useLocalState(initState, localKey) {
-  return null;
+  const [state, setState] = useState(() => {
+    const savedState = window.localStorage.getItem(localKey);
+
+    if (savedState !== null) return JSON.parse(savedState);
+    return initState;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(localKey, JSON.stringify(state))
+  }, [localKey, state]);
+
+  return [state, setState];
 }
